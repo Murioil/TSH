12bfe72eda0d6192bb32e706049155e64518cc9d
Murioil <>
2024-06-04 11:11:48 -0700

    update

--- TSH.sol
+++ TSH.sol
@@ -4,12 +4,10 @@ pragma solidity = 0.8.4;
 //WARNING: Do not assume this coin will operate the same with most standard contracts!
 //This has an IERC20 interface but this is a completely revolutionary kind of coin with a variable supply.
 interface TSH {
-    function decimals() external view returns (uint256);
+    function decimals() external view returns (uint8);
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
-    function increaseAllowance(address spender, uint value) external returns (bool);
-    function decreaseAllowance(address spender, uint value) external returns (bool);
     function transfer(address recipient, uint256 amount) external returns (bool);
     function approve(address spender, uint256 amount) external returns (bool);
     function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
@@ -20,6 +18,7 @@ contract CTSH is TSH {
     string public constant name     = "eShilling";
     string public constant symbol   = "TSH";
     string public version  = "1";
+    uint8 public override decimals = 8;
        
     address public minter;
     address public proxy; //Where all the coin functions and storage are
@@ -28,8 +27,7 @@ contract CTSH is TSH {
     event Approval(address indexed from, address indexed to, uint amount);
     event Transfer(address indexed from, address indexed to, uint amount);
 
-    // bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address holder,address spender,uint256 nonce,uint256 expiry,bool allowed)");
-    bytes32 public constant PERMIT_TYPEHASH = 0xea2aa0a1be11a07ed86d755c93467f4f82362b452371d1ba94d1715123511acb;
+    bytes32 public constant PERMIT_TYPEHASH = keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public DOMAIN_SEPARATOR;
     mapping (address => uint) public nonces;
     
@@ -50,11 +48,16 @@ contract CTSH is TSH {
     }
     function setProxy(address prox) public {
         require(msg.sender == minter);
-        if(proxy == address(0)) {            
+        if(proxy == address(0)) {
             proxy = prox;
         } else {
             if(proposedProxy != address(0)) {
                 require(block.timestamp > proxylock);
+                bool success;
+                bytes memory result;
+                uint thetime = type(uint).max;
+                (success, result) = proxy.staticcall(abi.encodeWithSignature("pauseContract(uint)",thetime));
+                require(success);
                 proxy = proposedProxy;
             }
         }
@@ -65,14 +68,6 @@ contract CTSH is TSH {
 
     //ERC20 Functions
     //Note: Solidity does not allow spaces between parameters in abi function calls
-    function decimals() public virtual override view returns (uint) {
-        bool success;
-        bytes memory result;
-        (success, result) = proxy.staticcall(abi.encodeWithSignature("decimals()"));
-        require(success);
-        uint _decimals = abi.decode(result, (uint));
-        return _decimals;
-    }
     function totalSupply() public virtual override view returns (uint) {
         bool success;
         bytes memory result;
@@ -88,14 +83,14 @@ contract CTSH is TSH {
         require(success);
         uint liquid = abi.decode(result, (uint));
         return liquid;
-    }    
+    }
     function allowance(address owner, address spender) public virtual override view returns (uint) {
         bool success;
         bytes memory result;
         (success, result) = proxy.staticcall(abi.encodeWithSignature("allowance(address,address)",owner,spender));
         require(success);
         return abi.decode(result, (uint));
-    }    
+    }
     function approve(address spender, uint value) public virtual override returns (bool) {
         require(spender != address(0));
         bool success;
@@ -104,25 +99,7 @@ contract CTSH is TSH {
         require(success);
         emit Approval(msg.sender, spender, value);
         return true;
-    }    
-    function increaseAllowance(address spender, uint value) public virtual override returns (bool) {
-        require(spender != address(0));
-        bool success;
-        bytes memory result;
-        (success, result) = proxy.call(abi.encodeWithSignature("increaseAllowance(address,uint256,address,uint256)",spender,value,msg.sender,0));
-        require(success); 
-        emit Approval(msg.sender, spender, allowance(msg.sender, spender));
-        return true;
-    }    
-    function decreaseAllowance(address spender, uint value) public virtual override returns (bool) {
-        require(spender != address(0));
-        bool success;
-        bytes memory result;
-        (success, result) = proxy.call(abi.encodeWithSignature("decreaseAllowance(address,uint256,address,uint256)",spender,value,msg.sender,0));
-        require(success); 
-        emit Approval(msg.sender, spender, allowance(msg.sender, spender));
-        return true;
-    }    
+    } 
     function transfer(address to, uint value) public virtual override returns (bool) {
         bool success;
         bytes memory result;
@@ -130,7 +107,7 @@ contract CTSH is TSH {
         require(success);
         emit Transfer(msg.sender, to, value);
         return true;
-    }    
+    }
     function transferFrom(address from, address to, uint value) public virtual override returns (bool) {
         bool success;
         bytes memory result;
@@ -140,15 +117,15 @@ contract CTSH is TSH {
         return true;
     }
     // --- Approve by signature ---
-    function permit(address holder, address spender, uint256 nonce, uint256 expiry, bool allowed, uint8 v, bytes32 r, bytes32 s) external {
-        bytes32 digest = keccak256(abi.encodePacked("\x19\x01",DOMAIN_SEPARATOR,keccak256(abi.encode(PERMIT_TYPEHASH,holder,spender,nonce,expiry,allowed))));
+    function permit(address holder, address spender, uint256 value, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) external {
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01",DOMAIN_SEPARATOR,keccak256(abi.encode(PERMIT_TYPEHASH,holder,spender,value,nonce,expiry))));
         require(holder != address(0), "Invalid-address");
         require(holder == ecrecover(digest, v, r, s), "Invalid-permit");
         require(expiry == 0 || block.timestamp <= expiry, "Permit-expired");
         require(nonce == nonces[holder], "Invalid-nonce");
         require(spender != address(0));
         nonces[holder]+=1;
-        uint wad = allowed ? type(uint256).max : 0;        
+        uint wad = value;        
         bool success;
         bytes memory result;
         (success, result) = proxy.call(abi.encodeWithSignature("approve(address,uint256,address,uint256)",spender,wad,holder,0));
--- shilling.sol
+++ shilling.sol
@@ -5,29 +5,29 @@ interface TSHDATA {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account, address m_sender) external view returns (uint256);
     function allowance(address owner, address spender) external view returns (uint256);
-    function increaseAllowance(address spender, uint256 value, address m_sender, uint _type) external returns (bool);
-    function decreaseAllowance(address spender, uint256 value, address m_sender, uint _type) external returns (bool);
+    function increaseAllowance(address spender, uint256 value, address m_sender, uint8 _type) external returns (bool);
+    function decreaseAllowance(address spender, uint256 value, address m_sender, uint8 _type) external returns (bool);
     function sendLiquid(address from, address to, uint256 amount, address m_sender) external returns (bool);
-    function approve(address spender, uint256 amount, address m_sender, uint _type) external returns (bool);
+    function approve(address spender, uint256 amount, address m_sender, uint8 _type) external returns (bool);
     function mint(address to, uint256 value) external returns (bool);
     function burn(address to, uint256 value) external returns (bool);
     function changeMinter(address newminter) external returns (bool);
-    function pauseContract(uint duration) external returns (bool);
+    function pauseContract(uint256 duration) external returns (bool);
 }
 
 contract TSH {
     string public name = 'eShilling data';
     string public symbol = 'TSH';
     string  public version = '1';
-    uint public decimals = 18;
-    uint public totalSupply;
-    mapping(address => uint) public balanceOfUser;
-    mapping(address => mapping(address => uint)) public allowance;
+    uint8 public decimals = 8;
+    uint80 public totalSupply;
+    mapping(address => uint80) public balanceOfUser;
+    mapping(address => mapping(address => uint80)) public allowance;
     mapping(address => bool) public isUser;
     mapping(uint => address[]) public auditAddresses;
-    mapping(uint => uint[]) public auditAmounts;
+    mapping(uint => uint80[]) public auditAmounts;
     mapping(uint => address[]) public auditAddresses2;
-    mapping(uint => uint[]) public auditAmounts2;
+    mapping(uint => uint80[]) public auditAmounts2;
     mapping(uint => uint) public nonceTime;
     uint public lastTimestamp;
     uint public auditnonce;
@@ -41,7 +41,14 @@ contract TSH {
     constructor() {
         minter = msg.sender;
     }
-
+    
+    function safeConvert(uint256 data) internal pure returns (uint80) {
+        if(data == type(uint256).max) {
+            data = type(uint80).max;
+        }
+        require(data <= type(uint80).max, "Overflow error");
+        return uint80(data);
+    }
     function changeMinter(address newminter) external returns (bool) {
         require (msg.sender == minter);
         minter = newminter;
@@ -54,12 +61,16 @@ contract TSH {
         return true;
     }
     function pauseContract(uint duration) external returns (bool) {
+        if(msg.sender == proxy || paused == type(uint256).max) {
+            paused = type(uint256).max;
+            return true;
+        }
         require (msg.sender == minter);
         require (duration < 2592000);
         paused = block.timestamp + duration;
         return true;
     }
-    function getLen(uint which, uint thenonce) public view returns (uint mylen) {
+    function getLen(uint8 which, uint thenonce) public view returns (uint mylen) {
         if(which == 0) {
             return users.length;
         }
@@ -73,6 +84,7 @@ contract TSH {
     function mint(address to, uint value) external returns (bool) {
         require (msg.sender == minter);
         require (paused < block.timestamp);
+        uint80 newvalue = safeConvert(value);
         if(!isUser[to]) {
             isUser[to] = true;
             users.push(to);
@@ -83,46 +95,50 @@ contract TSH {
             nonceTime[auditnonce] = block.timestamp;
         }
         auditAddresses[auditnonce].push(to);
-        auditAmounts[auditnonce].push(value);
-        totalSupply += value;
-        balanceOfUser[to] += value;
-        emit Transfer(address(0), to, value);
+        auditAmounts[auditnonce].push(newvalue);
+        totalSupply += newvalue;
+        balanceOfUser[to] += newvalue;
+        emit Transfer(address(0), to, uint(newvalue));
         return true;
     }
     function burn(address from, uint value) external returns (bool) {
         require (msg.sender == minter);
         require (paused < block.timestamp);
+        require (from == minter);
+        uint80 newvalue = safeConvert(value);
         if(block.timestamp + 604800 > lastTimestamp) {
             lastTimestamp = block.timestamp;
             auditnonce += 1;
             nonceTime[auditnonce] = block.timestamp;
         }
         auditAddresses2[auditnonce].push(from);
-        auditAmounts2[auditnonce].push(value);
-        totalSupply -= value;
-        balanceOfUser[from] -= value;
-        emit Transfer(from, address(0), value);
+        auditAmounts2[auditnonce].push(newvalue);
+        totalSupply -= newvalue;
+        balanceOfUser[from] -= newvalue;
+        emit Transfer(from, address(0), uint(newvalue));
         return true;
-    }    
+    }
     function balanceOf(address user, address m_sender) public view returns (uint) {
         m_sender = address(0); //Placeholder depending on type of future upgrade
-        return balanceOfUser[user];
+        return uint(balanceOfUser[user]);
     }
     function _approve(address owner, address spender, uint value) private {
-        allowance[owner][spender] = value;
-        emit Approval(owner, spender, value);
+        uint80 newvalue = safeConvert(value);
+        allowance[owner][spender] = newvalue;
+        emit Approval(owner, spender, uint(newvalue));
     }
     function _transfer(address from, address to, uint value) private {
         require (paused < block.timestamp);
+        uint80 newvalue = safeConvert(value);
         if(!isUser[to]) {
             isUser[to] = true;
             users.push(to);
         }
-        balanceOfUser[from] -= value;
-        balanceOfUser[to] += value;
-        emit Transfer(from, to, value);
+        balanceOfUser[from] -= newvalue;
+        balanceOfUser[to] += newvalue;
+        emit Transfer(from, to, uint(newvalue));
     }
-    function approve(address spender, uint value, address proxyaddy, uint _type) external returns (bool) {
+    function approve(address spender, uint value, address proxyaddy, uint8 _type) external returns (bool) {
         address m_sender = msg.sender;
         _type = 0;
         if (msg.sender == proxy) {
@@ -132,25 +148,26 @@ contract TSH {
         _approve(m_sender, spender, value);
         return true;
     }
-    function increaseAllowance(address spender, uint value, address proxyaddy, uint _type) external returns (bool) {
+    function increaseAllowance(address spender, uint value, address proxyaddy, uint8 _type) external returns (bool) {
         address m_sender = msg.sender;
         _type = 0;
         if (msg.sender == proxy) {
             m_sender = proxyaddy;
         }
         require(m_sender != address(0));
-        allowance[m_sender][spender] += value;
-        emit Approval(m_sender, spender, allowance[m_sender][spender]);
+        allowance[m_sender][spender] += safeConvert(value);
+        emit Approval(m_sender, spender, uint(allowance[m_sender][spender]));
         return true;
     }    
-    function decreaseAllowance(address spender, uint value, address proxyaddy, uint _type) external returns (bool) {
+    function decreaseAllowance(address spender, uint value, address proxyaddy, uint8 _type) external returns (bool) {
         address m_sender = msg.sender;
         _type = 0;
         if (msg.sender == proxy) {
             m_sender = proxyaddy;
         }
-        allowance[m_sender][spender] -= value;
-        emit Approval(m_sender, spender, allowance[m_sender][spender]);
+        require(m_sender != address(0));        
+        allowance[m_sender][spender] -= safeConvert(value);
+        emit Approval(m_sender, spender, uint(allowance[m_sender][spender]));
         return true;
     }
     function sendLiquid(address from, address to, uint value, address proxyaddy) external returns (bool) {
@@ -159,7 +176,7 @@ contract TSH {
             m_sender = proxyaddy;
         }
         if(from != m_sender) {
-            allowance[from][m_sender] -= value;
+            allowance[from][m_sender] -= safeConvert(value);
         }
         _transfer(from, to, value);
         return true;
@@ -169,7 +186,7 @@ contract TSH {
         return true;
     }
     function transferFrom(address from, address to, uint value) external returns (bool) {
-        allowance[from][msg.sender] -= value;
+        allowance[from][msg.sender] -= safeConvert(value);
         _transfer(from, to, value);
         return true;
     }
